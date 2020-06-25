4fb659dcd4cb89145c8a773d074849a239a53f44
riddle <754561543@qq.com>
2020-06-25 10:34:29 +0800

    添加项目文件。

---
+++ GitTest/GitTest.h
@@ -0,0 +1,33 @@
+﻿
+// GitTest.h: GitTest 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CGitTestApp:
+// 有关此类的实现，请参阅 GitTest.cpp
+//
+
+class CGitTestApp : public CWinApp
+{
+public:
+	CGitTestApp() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGitTestApp theApp;
